1c98c04b4d0a947b227d2445ab786a9836ad6452
Ivana Piljic <piljic.ivana@gmail.com>
2020-03-20 16:57:31 +0000

    Finishing robofriends

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// RECIVING PROPS AND DESCRUTURING THEM IN CURLY BRACKETS
+const Card = ({name, email, id}) => {
+  return (
+    <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img  alt='robots' src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+    );
+}
+
+export default Card;
